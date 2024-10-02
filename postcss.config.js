41b3284484c2553a3f2a8d1fb191c58e4feeb76f
szubair22 <101330346+szubair22@users.noreply.github.com>
2024-10-02 12:11:34 -0500

    Fix Upload function with updated css

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // any other PostCSS plugins you want to use
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./styles.css",
+    "./script.js",
+    "./server.js",
+    "./package.json"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
